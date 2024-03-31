f4aa64df3048877693611e47a30cd26d5c57ce35
Fun117 <fun117.kun@gmail.com>
2024-03-31 13:02:18 +0900

    New Features and Minor Fixes

---
+++ src/install.js
@@ -0,0 +1,12 @@
+console.log(`
+┌───────────────────────────────────────────────────────┐
+│                                                       │
+│   \u001b[38;5;42m✓\u001b[0m scratch-auth-react                                │
+│                                                       │
+│   Thank you for installing!                           │
+│                                                       │
+│   Please follow the setup guide.                      │
+│   > \u001b[38;5;74mhttps://github.com/Fun117/scratch-auth-react\u001b[0m      │
+│                                                       │
+└───────────────────────────────────────────────────────┘
+`);
